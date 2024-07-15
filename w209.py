5ee385c9bb4d0ebcfcee739b3242e2fe5c9cae43
Xinyi Zhu <juliezhu212@berkeley.edu>
2024-07-14 18:34:59 -0700

    Working files

---
+++ w209.py
@@ -0,0 +1,33 @@
+from flask import Flask, render_template
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return render_template("home.html")
+
+@app.route("/dash_1")
+def dash_1():
+    return render_template("dash_1.html")
+
+@app.route("/dash_2")
+def dash_2():
+    return render_template("dash_2.html")
+
+@app.route("/dash_3")
+def dash_3():
+    return render_template("dash_3.html")
+
+@app.route("/report")
+def report():
+    return render_template("report.html")
+
+@app.route("/sources")
+def sources():
+    return render_template("sources.html")
+
+@app.route("/about")
+def about():
+    return render_template("about.html")
+
+if __name__ == "__main__":
+    app.run()
